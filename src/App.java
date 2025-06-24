056aa5bf1db16bf39ab05b8c255d3ac6b33f848e
aminata-sane <aminata-constance.sane@laplateforme.io>
2025-06-24 11:15:39 +0200

    Initialization and User Login

---
+++ src/App.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        // Creates a Scanner object to read user input.
+        Scanner scanner = new Scanner(System.in);
+
+        // Infinite loop so that the program continues to request conversions
+        while (true) {
+            System.out.println("\nEntrez la chaîne de caractère à convertir (ou 'Q' pour arrêter) :");
+            String inputString = scanner.nextLine();
+
+            //Condition for exiting the loop 
+            if (inputString.equalsIgnoreCase("Q")) {
+                break;
+            }
+
+            System.out.println("Entrez la base de destination (ex: hexadecimal, binary, text) :");
+            String targetBase = scanner.nextLine();
+
+            //For now, we are just displaying what we have received.
+            System.out.println("Chaîne saisie :'" + inputString + "'");
+            System.out.println("Base choisie : '" + targetBase + "'");
+
+            // ---- WE WILL ADD THE CONVERSION LOGIC LATER HERE ----
+        }
+
+        // Close the scanner to free up resources. 
+        scanner.close();
+        System.out.println("Programme terminé.");
+    }  
+}
