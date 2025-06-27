509fdd2c2883d78ab9278986e6bc18407ad4b937
aminata-sane <aminata-constance.sane@laplateforme.io>
2025-06-27 16:29:32 +0200

    checking and adjusting

--- src/DecimalConverter.java
+++ src/DecimalConverter.java
@@ -1,7 +1,18 @@
 public class DecimalConverter implements BaseConverter {
     @Override
     public String convert(int asciiValue) {
-        return Integer.toString(asciiValue);
+        // Manually converting an integer to a string
+        if (asciiValue == 0) return "0";
+        String result = "";
+        int value = asciiValue;
+        boolean isNegative = value < 0;
+        if (isNegative) value = -value;
+        while (value > 0) {
+            result = (char)('0' + (value % 10)) + result;
+            value /= 10;
+        }
+        if (isNegative) result = "-" + result;
+        return result;
     }
 
     @Override
